f8377de0def2de4e029b03b59bead11d60bddc99
Anamaya Garodia <agarodia@edu.uwaterloo.ca>
2017-06-02 23:54:51 -0400

    Added functions and tests for better clarity

--- deque.py
+++ deque.py
@@ -1,4 +1,5 @@
-class deque(object):
+""" A deque or a 'doubly ended queue' is a queue where you can add and remove from both ends."""
+class deque:
 
     def __init__(self):
         self.items = []
@@ -21,22 +22,51 @@ class deque(object):
     def isEmpty(self):
         return self.items==[]
 
-class TestingDeque(object):
+    def getFront(self):
+    	return self.items[0]
+
+    def getBack(self):
+    	return self.items[len(self.items)-1]
+
+    def printItems(self):
+    	for index, item in enumerate(self.items):
+    		if(index==0):
+    			print 'Front: ', item
+    		elif(index==len(self.items)-1):
+    			print 'Back: ' , item
+    		else:
+    			print(item)
+    	if(self.items==[]):
+    		print 'Empty'
+    	print("--------")
+
+class TestingDeque:
 
     def Client(self):
         s = deque()
         assert(s.isEmpty()==True)
         s.addFront(1)
+        s.printItems()
         assert(s.size()==1)
+        assert(s.getFront()==1)
         s.addBack(2)
+        s.printItems()
         assert(s.size()==2)
+        assert(s.getBack()==2)
         s.addFront(0)
+        s.printItems()
         assert(s.size()==3)
+        assert(s.getFront()==0)
         s.removeFront()
+        s.printItems()
         assert(s.size()==2)
+        assert(s.getFront()==1)
         s.removeBack()
+        s.printItems()
         assert(s.size()==1)
+        assert(s.getBack()==1)
         s.removeFront()
+        s.printItems()
         assert(s.size()==0)
         assert(s.isEmpty()==True)
         print("ALL TEST CASES PASSED")
